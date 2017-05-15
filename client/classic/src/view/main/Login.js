594a1d90023853ba400efc939a78d59471793eb0
den1so <denrashodchikov@ya.ru>
2017-05-15 20:20:47 +0300

    SQL queries

---
+++ client/classic/src/view/main/Login.js
@@ -0,0 +1,28 @@
+﻿
+Ext.create('Ext.window.Window', {
+    title: 'Authorization',
+    height: 180,
+    width: 300,
+    layout: 'fit',
+    items: {
+        xtype: 'form',
+        reference: 'form',
+        items: [            {
+            xtype: 'textfield',
+            name: 'username',
+            fieldLabel: 'Login',
+            allowBlank: false
+        }, {
+            xtype: 'textfield',
+            name: 'password',
+            inputType: 'password',
+            fieldLabel: 'Password',
+            allowBlank: false
+        }
+        ],
+        buttons: [{
+            text: 'Login',
+            formBind: true,
+        }]
+    }
+}).show();
